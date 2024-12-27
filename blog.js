4b35c5fb09dd347994bed86ca18651f526c6fee7
Ha0-Z <inbox@haozhang.se>
2024-12-27 22:41:03 +0100

    Inplemented basic of blog page with stylesheet and javascript code.

---
+++ blog.js
@@ -0,0 +1,9 @@
+function displayBlog(blogId) {
+    document.getElementById('main-hover').style.display = "block";
+    document.body.style.overflow = "hidden";
+}
+
+function hideBlog() {
+    document.getElementById('main-hover').style.display = "none";
+    document.body.style.overflow = "auto";
+}
